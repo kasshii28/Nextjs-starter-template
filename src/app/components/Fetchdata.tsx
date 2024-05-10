d3f0e74b190b14707549841719eaeba2fb98811a
kasshii28 <gp22a043@oecu.jp>
2024-05-10 14:07:10 +0900

    初期構成の作成frontend

---
+++ src/app/components/Fetchdata.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { type Data } from '../types/data'
+
+const Fetchdata = ({message}: Data) => {
+  return (
+    <div>{message}</div>
+  )
+}
+
+export default Fetchdata
